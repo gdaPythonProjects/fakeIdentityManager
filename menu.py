e9c2522fd8c292f2780f7716c9ada33387a26717
Filip Breska <filixed@gmail.com>
2018-12-26 17:53:46 +0100

    Menu options

---
+++ menu.py
@@ -0,0 +1,12 @@
+#menu options
+
+class Menu:
+    def __init__(self):
+        self.menuList = (
+            1, "Create new identity",
+            2, "Create new random identity"
+        )
+
+
+    def get(self):
+        return self.menuList
