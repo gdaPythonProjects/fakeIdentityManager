6818c262ea6ab7a3bf370cee7f093ac1ec913a6b
Filip Breska <filixed@gmail.com>
2018-12-26 20:02:27 +0100

    corrected display

--- exec/display.py
+++ exec/display.py
@@ -4,5 +4,8 @@ class Display:
     def __init__(self):
         dis = Menu()
         self.list = dis.get()
-        print(self.list)
-
+        for i in range(0, len(self.list)):
+            if i % 2 == 0:
+                print(str(self.list[i]) + ". ", end='')
+            else:
+                print(self.list[i])
