7fa66e521661966f051bf4926b951af2f45a9fc5
filixed <filixed@gmail.com>
2019-01-08 17:44:57 +0100

    gender handling and reading surnames

--- exec/fakeIDgenerator.py
+++ exec/fakeIDgenerator.py
@@ -3,21 +3,33 @@ import random
 import numpy as np
 
 class fakeidgen:
-    def __init__(self):
-        self.name = self.setname()
+    def __init__(self, gender):
+        self.name = self.setname(gender)
         self.surName = self.setsurename()
         self.pesel = self.setpesel()
         self.email = self.setemail()
 
-    def setname(self):
-        with open('name_f.txt') as f:
-            names = f.read().splitlines()
+        print(self.name, self.surName)
+
+    def setname(self, gender):
+        if gender == 1:
+            with open('exec/name_f.txt') as f:
+                names = f.read().splitlines()
+                f.close()
+        else:
+            with open('exec/name_m.txt') as f:
+                names = f.read().splitlines()
+                f.close()
         arr = np.asarray(names)
         name = arr[random.randint(0, len(arr))]
         return name
 
     def setsurename(self):
-        pass
+        with open('exec/surname.txt') as f:
+            names = f.read().splitlines()
+        arr = np.asarray(names)
+        name = arr[random.randint(0, len(arr))]
+        return name
 
     def setpesel(self):
         pass
@@ -27,7 +39,7 @@ class fakeidgen:
 
 
 
-def nameRand():
+
 
 
 
--- main.py
+++ main.py
@@ -3,10 +3,13 @@
 from cmdinput import cmdInput
 from exec.display import Display
 from exec.fakeID import fakeID
+from exec.fakeIDgenerator import fakeidgen
 
 if __name__ == '__main__':
     Display = Display()
     cmdInput = cmdInput()
     if cmdInput.uArgs == '1':
         fakeID = fakeID()
+    else:
+        fakeidgen = fakeidgen(input("Enter 1 for female anything for male: "))
 
